ad12be4416abea1dc5a83dd494258b40c2202618
Benjamin <haomingkai@gmail.com>
2018-06-13 22:12:31 -0700

    publish my resume online

---
+++ js/index.js
@@ -0,0 +1,316 @@
+$(function() {
+	var c = new Coder("style-text");
+	c.load("<p><div class='bef_comment writecode'>/**</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*  Hi,I'm Benjamin Hao. I am a graduate of British Columbia Institute of Technology.</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*  I'm from Computer Systems Technology - Web and Mobile Option</div>",2000);
+	c.load("<div class='bef_comment writecode'>&nbsp;*  Let's see what I can do.</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*/</div></p>");
+	c.load("<p><div class='bef_comment writecode'>/**</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*  Shall we begin? Let's animate everything.</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*/</div></p>");
+	c.load("<p><div class='code writecode'><span class='bef_selector'>*</span> {</div>");
+	c.load("<div class='code writecode'>&nbsp;<span class='bef_key'>-webkit-transition</span>: <span class='bef_value'> all 1s</span>;</div>");
+	c.load("<div class='code writecode'>}</div></p>");
+	c.load("<p><div class='bef_comment writecode'>/**</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*  Nothing different? Don't worry. Let's wait and see.</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*  It's so boring with only black and white.</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*  Let's make some changes.</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*/</div></p>");
+	c.load("<p><div class='code writecode'><span class='bef_selector'>html</span> {</div>");
+	c.load("<div class='code writecode'>&nbsp;<span class='bef_key'>background</span>: <span class='bef_value'>rgb(63, 82, 99)</span>;</div>");
+	c.setClass("html", [{
+		"name": "background",
+		"val": "rgb(63, 82, 99)"
+	}]);
+	c.load("<div class='code writecode'>}</div></p>");
+	c.load("<p><div class='bef_comment writecode'>/**</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*  And... The font colour.</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*/</div></p>");
+	c.load("<p><div class='code writecode'><span class='bef_selector'>pre,a</span> {</div>");
+	c.load("<div class='code writecode'>&nbsp;<span class='bef_key'>color</span>: <span class='bef_value'>white</span>;</div>");
+	c.setClass("pre,a", [{
+		"name": "color",
+		"val": "white"
+	}]);
+	c.load("<div class='code writecode'>}</div></p>");
+	c.load("<p><div class='bef_comment writecode'>/**</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*  It looks much better, right?</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*  But I don't like writing code on the left side of the screen.</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*  I need a better area to display my code.</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*/</div></p>");
+	c.load("<p><div class='code writecode'><span class='bef_selector'>pre</span><span class='bef_key'>:not(:empty)</span> {</div>");
+	c.load("<div class='code writecode'>&nbsp;<span class='bef_key'>overflow</span>: <span class='bef_value'>hidden</span>;</div>");
+	c.setClass("pre:not(:empty)", [{
+		"name": "overflow",
+		"val": "hidden"
+	}]);
+	c.load("<div class='code writecode'>&nbsp;<span class='bef_key'>background</span>: <span class='bef_value'>rgb(48, 48, 48)</span>;</div>");
+	c.setClass("pre:not(:empty)", [{
+		"name": "background",
+		"val": "rgb(48, 48, 48)"
+	}]);
+	c.load("<div class='code writecode'>&nbsp;<span class='bef_key'>border</span>: <span class='bef_value'>1px solid #ccc</span>;</div>");
+	c.setClass("pre:not(:empty)", [{
+		"name": "border",
+		"val": "1px solid #ccc"
+	}]);
+	c.load("<div class='code writecode'>&nbsp;<span class='bef_key'>max-height</span>: <span class='bef_value'>44.6%</span>;</div>");
+	c.setClass("pre:not(:empty)", [{
+		"name": "max-height",
+		"val": "44.6%"
+	}]);
+	c.load("<div class='code writecode'>&nbsp;<span class='bef_key'>width</span>: <span class='bef_value'>49%</span>;</div>");
+	c.setClass("pre:not(:empty)", [{
+		"name": "width",
+		"val": "49%"
+	}]);
+	$('#pre:not(:empty)').addClass("toarea");
+	c.load("<div class='code writecode'>&nbsp;<span class='bef_key'>font-size</span>: <span class='bef_value'>14<span class='bef_px'>px</span></span>;</div>");
+	c.setClass("pre:not(:empty)", [{
+		"name": "font-size",
+		"val": "14px"
+	}]);
+	c.load("<div class='code writecode'>&nbsp;<span class='bef_key'>font-family</span>: <span class='bef_value'>monospace</span>;</div>");
+	c.setClass("pre:not(:empty)", [{
+		"name": "font-family",
+		"val": "monospace"
+	}]);
+	c.load("<div class='code writecode'>&nbsp;<span class='bef_key'>padding</span>: <span class='bef_value'>10<span class='bef_px'>px</span> 10<span class='bef_px'>px</span> 20<span class='bef_px'>px</span></span>;</div>");
+	c.setClass("pre:not(:empty)", [{
+		"name": "padding",
+		"val": "10px 10px 20px"
+	}]);
+	c.load("<div class='code writecode'>&nbsp;<span class='bef_key'>box-shadow</span>: <span class='bef_value'>-4<span class='bef_px'>px</span> 4<span class='bef_px'>px</span> 2<span class='bef_px'>px</span> 0 rgba(0,0,0,0.3)</span>;</div>");
+	c.setClass("pre:not(:empty)", [{
+		"name": "box-shadow",
+		"val": "-4px 4px 2px 0 rgba(0,0,0,0.3)"
+	}]);
+	c.load("<div class='code writecode'>&nbsp;<span class='bef_key'>white-space</span>: <span class='bef_value'>pre-wrap</span>;</div>");
+	c.setClass("pre:not(:empty)", [{
+		"name": "white-space",
+		"val": "pre-wrap"
+	}]);
+	c.load("<div class='code writecode'>&nbsp;<span class='bef_key'>outline</span>: <span class='bef_value'>0</span>;</div>");
+	c.setClass("pre:not(:empty)", [{
+		"name": "outline",
+		"val": "0"
+	}]);
+	c.load("<div class='code writecode'>}</div></p>");
+	c.load("<p><div class='bef_comment writecode'>/**</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*  OK，let's move this a little bit.</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*  And make some more changes.</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*/</div></p>");
+	c.load("<p><div class='code writecode'><span class='bef_selector'>#style-text</span> {</div>");
+  	c.load("<div class='code writecode'>&nbsp;<span class='bef_key'>-webkit-transform</span>: <span class='bef_value'>translateX(95%)</span>;</div>");
+  	c.setClass("#style-text", [{
+		"name": "-webkit-transform",
+		"val": "translateX(95%)"
+	}]);
+  	c.load("<div class='code writecode'>&nbsp;<span class='bef_key'>position</span>: <span class='bef_value'>absolute</span>;</div>");
+  	c.setClass("#style-text", [{
+		"name": "position",
+		"val": "absolute"
+	}]);
+	c.load("<div class='code writecode'>}</div></p>");
+	c.load("<p><div class='bef_comment writecode'>/**</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*  It looks much better, but I'm not going to code with Notepad.</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*  Let's make it more readable.</div>");
+	c.load("<div class='bef_comment writecode'>&nbsp;*/</div></p>");
+	c.load("<p><div class='code writecode'><span class='bef_selector'>.comment</span>&nbsp;{ <span class='bef_key'>color</span>: <span class='bef_value'>#857F6B</span>; <span class='bef_key'>font-style</span>: <span class='bef_value'>italic</span>; }</div>");
+	c.addClass(".bef_comment","comment");
+	c.load("<div class='code writecode'><span class='bef_selector'>.selector</span>&nbsp;{ <span class='bef_key'>color</span>: <span class='bef_value'>#E69F0F</span>; }</div>");
+	c.addClass(".bef_selector","selector");
+	c.load("<div class='code writecode'><span class='selector'>.selector .key</span>&nbsp;{ <span class='bef_key'>color</span>: <span class='bef_value'>#64D5EA</span>; }</div>");
+	c.addClass(".bef_key","key");
+	c.load("<div class='code writecode'><span class='selector'>.key</span>&nbsp;{ <span class='key'>color</span>: <span class='bef_value'>#64D5EA</span>; }</div>");
+	c.load("<div class='code writecode'><span class='selector'>.value</span>&nbsp;{ <span class='key'>color</span>: <span class='bef_value'>#BE84F2</span>; }</div>");
+	c.addClass(".bef_value","value");
+	c.load("<div class='code writecode'><span class='selector'>.value.px</span>&nbsp;{ <span class='key'>color</span>: <span class='value'>#F92772</span>; }</div></p>");
+	c.addClass(".value .bef_px","px");
+	c.load("<p><div class='comment writecode'>/**</div>");
+	c.load("<div class='comment writecode'>&nbsp;*  Now I'm ready.</div>");
+	c.load("<div class='comment writecode'>&nbsp;*  Let's rotate this a little bit.</div>");
+	c.load("<div class='comment writecode'>&nbsp;*/</div></p>");
+	c.load("<p><div class='code writecode'><span class='selector'>#content</span> {</div>");
+  	c.load("<div class='code writecode'>&nbsp;<span class='key'>-webkit-perspective</span>: <span class='value'>1000<span class='px'>px</span></span>;</div>");
+  	c.setClass("#content", [{
+		"name": "-webkit-perspective",
+		"val": "1000px"
+	}]);
+  	c.load("<div class='code writecode'>}</div></p>");
+  	c.load("<p><div class='code writecode'><span class='selector'>#style-text</span> {</div>");
+  	c.load("<div class='code writecode'>&nbsp;<span class='key'>-webkit-transform</span>: <span class='value'>translateX(98.5%) rotateY(-10deg)</span>;</div>");
+  	c.setClass("#style-text", [{
+		"name": "-webkit-transform",
+		"val": "translateX(98.5%) rotateY(-10deg)"
+	}]);
+	c.load("<div class='code writecode'>&nbsp;<span class='key'>-webkit-transform-origin</span>: <span class='value'>right</span>;</div>");
+  	c.setClass("#style-text", [{
+		"name": "-webkit-transform-origin",
+		"val": "right"
+	}]);
+	c.load("<div class='code writecode'>&nbsp;<span class='key'>max-height</span>: <span class='value'>94.5%</span>;</div>");
+  	c.setClass("#style-text", [{
+		"name": "max-height",
+		"val": "94.5%"
+	}]);
+  	c.load("<div class='code writecode'>}</div></p>");
+  	c.load("<p><div class='comment writecode'>/**</div>");
+	c.load("<div class='comment writecode'>&nbsp;*  I'm pretty sure you came here not to check these JavaScript and CSS stuff</div>");
+	c.load("<div class='comment writecode'>&nbsp;*  So here is my resume.</div>");
+	c.load("<div class='comment writecode'>&nbsp;*/</div></p>");
+	c.addClass("#work-text","pre_work-text");
+	c.load("<p><div class='code writecode'><span class='selector'>pre</span><span class='key'>:not(#style-text)</span> { </div>");
+	c.load("<div class='code writecode'>&nbsp;<span class='key'>-webkit-transform</span>: <span class='value'>rotateY(10deg)</span>;</div>");
+	c.load("<div class='code writecode'>&nbsp;<span class='key'>-webkit-transform-origin</span>: <span class='value'>left</span>;</div>");
+	c.load("<div class='code writecode'>}</div></p>");
+	var r = new Coder("work-text");
+	r.load("<div id='md'></div>");
+	var w = new Coder("md");
+	w.load("<h1 class='writecode'>Benjamin(Mingkai) Hao</h1>");
+	w.load("<div class='writecode'>Frontend/Mobile App Developer</div>");
+	w.load("<div class='writecode'>Graduate of British Columbia Institute of Technology <br/>&nbsp;&nbsp;- Computer Systems Technology Diploma Program, Web and Mobile Option</div></p>");
+	w.load("<h1 class='writecode'>Contact Me</h1>");
+	w.load("<ul>");
+	w.load("<li class='writecode'>Email: haomingkai@gmail.com</li>");
+	w.load("<li class='writecode'>Phone: (778)323-0400</li>");
+	w.load("</ul>");
+	w.load("<h1 class='writecode'>Skills & Technologies</h1>");
+	w.load("<ul>");
+	w.load("<li class='writecode'>Excellent in HTML5, CSS, JavaScript, PHP, Swift, Java, C#, .NET Framework, SQL</li>");
+	w.load("<li class='writecode'>Proficient in C, C++, TypeScript, jQuery, Laravel, Angular, Linux</li>");
+	w.load("<li class='writecode'>Familiar with most of popular software and open source technologies such as <br/>&nbsp;&nbsp;&nbsp;&nbsp; GitHub, Docker, Unity, Slack, Visual Studio, XCode, Android Studio, Vim, etc.</li>");
+	w.load("<li class='writecode'>Have experience on real-world industry projects, familiar with agile development.</li>");
+	w.load("<li class='writecode'>Fluent in Mandarin, familiar with Chinese software and games market.</li>");
+	w.load("</ul>");
+	w.load("<h1 class='writecode'>Technical Projects</h1>");
+	w.load("<ul>");
+	w.load("<div class='writecode'>")
+	w.load("<li><a href='https://github.com/BenjaminHao/MyLaravelBlog' target='_blank'>Personal Laravel Blog (Personal project)</a>");
+	w.load("&nbsp;&nbsp;&nbsp;&nbsp;✔A personal blog built with PHP/SQLite/Laravel framework/JavaScript/CSS/HTML");
+	w.load("<br>&nbsp;&nbsp;&nbsp;&nbsp;✔Used Ajax and JSON wherever possible to prevent page refresh");
+	w.load("<br>&nbsp;&nbsp;&nbsp;&nbsp;✔Used version control: GitHub, web server: Apache</li>");
+	w.load("<li class='writecode'><a href='https://github.com/BenjaminHao/GettingOverAd' target='_blank'>Getting Over Ad (Personal project)</a>");
+	w.load("&nbsp;&nbsp;&nbsp;&nbsp;✔A challenging endless iOS jumping game developed with Swift 4");
+	w.load("<br>&nbsp;&nbsp;&nbsp;&nbsp;✔Have Published to App Store, familiar with iOS apps development workflow");
+	w.load("<br>&nbsp;&nbsp;&nbsp;&nbsp;✔Feature: Pure hardcoding app, object-oriented programing, Sprite Kit, Google AdMob</li>");
+	w.load("<li class='writecode'><a href='https://github.com/BenjaminHao/CitySimulator2017' target='_blank'>City Simulator 2017 (Capstone project)</a>");
+	w.load("&nbsp;&nbsp;&nbsp;&nbsp;✔A client-server application as a class-wide capstone project");
+	w.load("<br>&nbsp;&nbsp;&nbsp;&nbsp;✔Built with Unity Engine and rendered with HoloLens");
+	w.load("<br>&nbsp;&nbsp;&nbsp;&nbsp;✔Use proper Git flow and Agile/Scum methodologies");
+	w.load("<br>&nbsp;&nbsp;&nbsp;&nbsp;✔Responsible for developing grid system, City Data Manager, Character BFS/Moving</li>");
+	w.load("<li class='writecode'>Slack Survey Bot (Real-world industry project)");
+	w.load("&nbsp;&nbsp;&nbsp;&nbsp;✔Bot that communicate with users and help them complete questions");
+	w.load("<br>&nbsp;&nbsp;&nbsp;&nbsp;✔Rely on the Slack API to handle communication with the end user, developed with TypeScript");
+	w.load("<br>&nbsp;&nbsp;&nbsp;&nbsp;✔Responsible for developing survey sending & reminder function</li>");
+	w.load("</div></ul>");
+	c.load("<p><div class='comment writecode'>/**</div>");
+	c.load("<div class='comment writecode'>&nbsp;* The resume is not very pretty. Let's do some animated rendering.</div>");
+	c.load("<div class='comment writecode'>&nbsp;*/</div></p>");
+	c.load("<p><div class='code writecode'><span class='selector'>#work-text</span> { </div>");
+	c.load("<div class='code writecode'>&nbsp;<span class='key'>-webkit-transform</span>: <span class='value'>rotateX(0deg) rotateY(190deg) rotateZ(180deg)</span>;</div>");
+	c.load("<div class='code writecode'>}</div></p>");
+	c.load("<p><div class='code writecode'><span class='selector'>#work-text #md</span> { </div>");
+	c.load("<div class='code writecode'>&nbsp;<span class='key'>-webkit-transform</span>: <span class='value'>rotateY(190deg) rotateZ(180deg)</span>;</div>");
+	c.load("<div class='code writecode'>}</div></p>");
+	c.load("<p><div class='comment writecode'>/**</div>");
+	c.load("<div class='comment writecode'>&nbsp;* Here we go！</div>");
+	c.load("<div class='comment writecode'>&nbsp;*/</div></p>");
+	c.setClass("#work-text", [{
+		"name": "-webkit-transform",
+		"val": "rotateX(0deg) rotateY(190deg) rotateZ(180deg)"
+	}]);
+	c.setClass("#work-text #md", [{
+		"name": "-webkit-transform",
+		"val": "rotateY(190deg) rotateZ(180deg)"
+	}]);
+	c.load("<p><div class='comment writecode'>/**</div>");
+	c.load("<div class='comment writecode'>&nbsp;* Oops, it seems I need to tweak these words a little bit.</div>");
+	c.load("<div class='comment writecode'>&nbsp;*/</div></p>");
+	c.load("<p><div class='code writecode'><span class='selector'>#md</span> { </div>");
+	c.load("<div class='code writecode'>&nbsp;<span class='key'>font-family</span>: <span class='value'>'Helvetica Neue', Helvetica, sans-serif</span>;</div>");
+	c.setClass("#md", [{
+		"name": "font-family",
+		"val": "'Helvetica Neue', Helvetica, sans-serif"
+	}]);
+	c.load("<div class='code writecode'>}</div></p>");
+	c.load("<p><div class='code writecode'><span class='selector'>#md h1</span> { </div>");
+	c.load("<div class='code writecode'>&nbsp;<span class='key'>border-bottom</span>: <span class='value'>1px solid #ccc</span>;</div>");
+	c.setClass("#md h1", [{
+		"name": "border-bottom",
+		"val": "1px solid #ccc"
+	}]);
+	c.load("<div class='code writecode'>&nbsp;<span class='key'>padding-bottom</span>: <span class='value'>5px</span>;</div>");
+	c.setClass("#md h1", [{
+		"name": "padding-bottom",
+		"val": "5px"
+	}]);
+	c.load("<div class='code writecode'>}</div></p>");
+	c.load("<p><div class='code writecode'><span class='selector'>#md li</span> { </div>");
+	c.load("<div class='code writecode'>&nbsp;<span class='key'>margin</span>: <span class='value'>5<span class='px'>px</span> 0</span>;</div>");
+	c.setClass("#md li", [{
+		"name": "margin",
+		"val": "5px 0"
+	}]);
+	c.load("<div class='code writecode'>}</div></p>");
+	c.load("<p><div class='code writecode'><span class='selector'>#md h1,#md ul</span> { </div>");
+	c.load("<div class='code writecode'>&nbsp;<span class='key'>margin</span>: <span class='value'>0</span>;</div>");
+	c.setClass("#md h1,#md ul", [{
+		"name": "margin",
+		"val": "0"
+	}]);
+	c.load("<div class='code writecode'>}</div></p>");
+	c.load("<p><div class='comment writecode'>/**</div>");
+	c.load("<div class='comment writecode'>&nbsp;* It looks fine now. :)</div>");
+	c.load("<div class='comment writecode'>&nbsp;*");
+	c.load("<div class='comment writecode'>&nbsp;* This is just a simple resume created with CSS and JavaScript, </div>");
+	c.load("<div class='comment writecode'>&nbsp;* I'm also passionate about software/game/Mobile App development!</div>");
+	c.load("<div class='comment writecode'>&nbsp;* If you want to contact me");
+	c.load("<div class='comment writecode'>&nbsp;* My Email: haomingkai@gmail.com</div>");
+	c.load("<div class='comment writecode'>&nbsp;* My Phone: 7783230400</div>");
+	c.load("<div class='comment writecode'>&nbsp;* Thank you very much for spending your precious time watching my resume!</div>");
+	c.load("<div class='comment writecode'>&nbsp;* Have a nice day and I’m looking forward to hearing from you!</div>");
+	c.load("<div class='comment writecode'>&nbsp;* </div>");
+	c.load("<div class='comment writecode'>&nbsp;* Comment by Benjamin Hao.</div>");
+	c.load("<div class='comment writecode'>&nbsp;* June 13rd, 2018</div>");
+	c.load("<div class='comment writecode'>&nbsp;*/</div></p>");
+
+});
+
+var m = 0;
+
+var Coder = function(dom) {
+	this._dom = dom;
+};
+
+Coder.prototype = {
+	constructor: Coder,
+	load: function(code, second) {
+		var dom = this._dom;
+		var o = this;
+		var _second = second ? second : 1000;
+		_second = m + _second;
+		setTimeout(function() {
+			$('#' + dom).scrollTop( $('#' + dom)[0].scrollHeight );
+			$("#" + dom).append(code);
+		}, _second);
+		m = _second;
+	},
+	setClass: function(dom, styles,second) {
+		var _second = second ? second : 0;
+		_second = m + _second;
+		for (var i in styles) {
+			setTimeout(function() {
+				$(dom).css(styles[i].name, styles[i].val);
+			}, _second);
+		}
+		m = _second;
+	},
+	addClass:function(dom,className){
+		var second = m;
+		setTimeout(function() {
+			$(dom).addClass(className);
+		}, second);
+	}
+}
